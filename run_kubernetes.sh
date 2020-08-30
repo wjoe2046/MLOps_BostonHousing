b417f6d0380ae96fa638833f9bbaf18120197d2e
Wenfei Zhou <wzhou2046@gmail.com>
2020-08-30 12:51:12 -0400

    edited kubernetes.sh script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=wjoe2046/mlboston:0.1 
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlboston --image=${dockerpath}
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlboston 8000:80
