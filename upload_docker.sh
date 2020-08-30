ce03b731a5032a41f23cca1cbc1688d941f8aab0
Wenfei Zhou <wzhou2046@gmail.com>
2020-08-30 12:31:52 -0400

    inserted login function in app,py; modified shell script to allow for uploading to dockerhub

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=wjoe2046/mlboston:0.1
 
 # Step 2:  
 # Authenticate & tag
+docker login --username=wjoe2046
+docker tag mlboston $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
